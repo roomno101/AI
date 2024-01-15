11ad53fa7e2304472cdf47dd50cefe64f4268909
roomno101 <156678535+roomno101@users.noreply.github.com>
2024-01-15 19:01:07 +0530

    Update sells_buys.pl

--- sells_buys.pl
+++ sells_buys.pl
@@ -8,3 +8,12 @@ buys(tony,bread).
 mita_sells(X) :- buys(neelam,X).
 tony_buys(X) :- sells(john,X).
 
+%output-
+?- sells(mita,X),buys(neelam,X).
+X = egg.
+?- buys(tony,bread),sells(X,bread).
+X = john.
+?- mita_sells(X).
+X = egg.
+?- tony_buys(X).
+X = bread.
